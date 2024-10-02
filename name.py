833abf273c471b71db27c10df4f8246e3b27743c
SoyGabo24 <gabriel.ccoicca.a@gmail.com>
2024-10-02 16:24:52 -0500

    Update name.py

--- name.py
+++ name.py
@@ -1,1 +1,15 @@
-print("Hola Mundo")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "GG": "Good Game",
+            "EZ": "Facilisimo",
+            "GL": "Good Luck",
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Esta palabra no está en nuestro diccionario")
